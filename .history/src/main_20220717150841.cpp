d848ff1f12700a9099f0a0326f4744198645291c
atx25 <89027995+atx25@users.noreply.github.com>
2022-07-17 23:41:42 +0800

    fddsfsdfdfdsfsfdsfs

---
+++ .history/src/main_20220717150841.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <ESP8266HTTPClient.h> 
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+const char* AP_SSID     = "HUAWEI-0409E6";         // XXXXXX -- 使用时请修改为当前你的 wifi ssid
+const char* AP_PSK = "15660090095";        // XXXXXX -- 使用时请修改为当前你的 wifi 密码
+WiFiClientSecure client; 
+HTTPClient https; //创建一个网络对象
+const char* location="110101";
+const char* key="d4055a0ac0848d29ab8bb6e4ad498d7a";
+const char* extensions;                    
+void wifi_start_connect()              //连接WIFI
+{
+  WiFi.mode(WIFI_STA);                 //设置esp8266 工作模式 
+  Serial.println("Connecting to ");    //写几句提示
+  Serial.println(AP_SSID);
+  WiFi.begin(AP_SSID, AP_PSK);         //连接wifi
+  WiFi.setAutoConnect(true);
+  while (WiFi.status()!= WL_CONNECTED) //这个函数是wifi连接状态，返回wifi链接状态
+        {  
+         delay(500);
+         Serial.print(".");
+        }
+  Serial.println("WiFi connected IP is");
+  Serial.println(WiFi.localIP()); 
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);//设置波特率
+  wifi_start_connect();//连接wifi
+  client.setTimeout(5000);//设置服务器连接超时
+  
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  client.setInsecure();
+  Serial.println("[HTTPS] begin...");
+  String url = "https://restapi.amap.com/v3/weather/weatherInfo?&city="
+                + location + "&key=" + key + "&extensions=" + extensions;
+  if(https.begin(client,url)){
+    Serial.println("[HTTPS] GET...");
+    int httpCode = https.GET();
+    if(httpCode >= 0){
+      Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) { // 服务器响应
+        String str = https.getString();
+        Serial.println(str);
+        
+      }
+    } else { // 错误返回负值
+      Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+    }
+    https.end();
+    }
+  else { // HTTPS连接失败
+    Serial.printf("[HTTPS] Unable to connect\n");
+  }  
+  delay(10000);
+}
